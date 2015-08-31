31d2d870c665acb9a627c2c3400e7bb3f2f14cfd
Hugo De Albuquerque Fonseca <hugoalbuquerque0@gmail.com>
2015-08-31 12:26:00 -0300

    adicionadas funcoes de conversao binaria e remocao zero a esquerda. falta criar funcao de divisao por 2

--- ex1.c
+++ ex1.c
@@ -1,2 +1,28 @@
 int main
 return 0;
+
+void dec2bin(char *numantes, char *numdepois)
+{
+    char tempchar[BUFFER];
+    if(*numantes == '\0')
+    {
+        *numdepois = '\0';
+        return;
+    }
+    *numdepois = div2(numantes, tempchar) + '0';
+    zeroleft(tempchar);
+    ++numdepois;
+    dec2bin(tempchar,numdepois);
+    return;
+}
+
+void zeroleft(char *numero)
+{
+    if(*numero == '0')
+        while(*numero != '\0')
+        {
+            *numero = *(numero + 1);
+            ++numero;
+        }
+    return;
+}
